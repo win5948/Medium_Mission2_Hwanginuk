362363542f9d3c6f0949fb1d34da7012c3e9b08a
win5948 <win5948@gmail.com>
2024-01-04 15:06:25 +0900

    미션3

--- src/main/java/com/ll/medium/global/initData/NotProd.java
+++ src/main/java/com/ll/medium/global/initData/NotProd.java
@@ -29,8 +29,17 @@ public class NotProd {
             @Transactional
             @Override
             public void run(ApplicationArguments args) throws Exception {
+                // 기존 사용자 존재 체크
                 if (memberService.findByUsername("user1").isPresent()) return;
 
+                // 유료 멤버십 회원 생성
+                for (int i = 1; i <= 100; i++) {
+                    Member paidMember = memberService.join("paidUser" + i, "1234").getData();
+                    paidMember.setRefreshToken("token" + i);
+                    paidMember.setPaid(true); // 유료 멤버십 설정
+                }
+
+                // 기존 사용자 생성
                 Member memberUser1 = memberService.join("user1", "1234").getData();
                 memberUser1.setRefreshToken("user1");
 
@@ -43,6 +52,12 @@ public class NotProd {
                 Member memberUser4 = memberService.join("user4", "1234").getData();
                 memberUser4.setRefreshToken("user4");
 
+                //유료 글 생성
+                for (int i = 1; i <= 100; i++) {
+                    postService.write(memberUser1, "유료 제목 " + i, "유료 내용 " + i, true);
+                }
+
+                //기존 글 생성
                 postService.write(memberUser1, "제목 1", "내용 1", true);
                 postService.write(memberUser1, "제목 2", "내용 2", true);
                 postService.write(memberUser1, "제목 3", "내용 3", false);
